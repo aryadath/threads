a7fdb3361cc14419505a551808d67b425c942e7a
Arya <aryadath@gmail.com>
2023-09-13 22:12:13 +0100

    Added navigation segments

--- app/(auth)/sign-in/sign-up/onboarding/layout.tsx
+++ app/(auth)/sign-in/sign-up/onboarding/layout.tsx
@@ -4,25 +4,18 @@ import { Inter } from "next/font/google"
 import '../globals.css';
 
 export const metadata = {
-    title : 'Threads',
+    title: 'Threads',
     description: 'A Next.js 13 Meta Threads Application'
 }
 
-const inter = Inter({ subsets: ["latin"]})
+const inter = Inter({ subsets: ["latin"] })
 
-export default function RootLayout({ 
-    children 
-}: { 
-    children: React.ReactNode 
- }) {
-    return(
-        <ClerkProvider>
-            <html lang="en">
-                <body className={`${inter.className} bg-dark-1` }>
-                    {children}
-                </body>
-            </html>
-
-        </ClerkProvider>
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+      
     )
 }
