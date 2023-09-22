c08470f400e2301ee96683233aeafef3b76bc2e0
Arya <aryadath@gmail.com>
2023-09-22 21:34:49 +0100

    Created a button for posting threads and getting responses

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -147,6 +147,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
               </FormControl>
+              <FormMessage/>
             </FormItem>
           )}
         />
